713fb917da5a4c994d1cb268a95c8f689496252e
Mariam Qusai <m.sagban@mailahoy.com>
2024-02-01 16:00:45 +0400

    Added a configuration file

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 import json
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from driver_config import configure_driver
 from scrape_functions import scrape_product_data
 from utils import load_urls
 
-file_path = "scraped_data.json"
-urls_file_path = 'products_urls.csv'
+def load_config():
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    return config
 
 def main():
+    config = load_config()
+    file_path = config["file_path"]
+    urls_file_path = config["urls_file_path"]
+    webdriver_timeout = config["webdriver_timeout"]
+
     products_urls = load_urls(urls_file_path)
     data = []
 
     try:
         driver = configure_driver()
-        wait = WebDriverWait(driver, 10)  # Adjust the timeout as needed
+        WebDriverWait(driver, webdriver_timeout) 
 
-        for url in products_urls[:6]:
+        for url in products_urls:
             product_data = scrape_product_data(url, driver)
             data.append(product_data)
 
