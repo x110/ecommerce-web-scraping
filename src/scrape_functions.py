ce8fd0374a45e158c5b850cd3509caede01518c5
x110 <masagban@gmail.com>
2024-02-02 08:55:14 +0400

    Handle network errors exception

--- src/scrape_functions.py
+++ src/scrape_functions.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.remote.webdriver import WebDriver
 from bs4 import BeautifulSoup
 import re
 
@@ -52,17 +53,35 @@ def find_gender(soup):
         print("Gender not found.")
         return None
 
-def scrape_product_data(url, driver):
-    driver.get(url)
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
+def scrape_product_data(url: str, driver: WebDriver) -> dict:
+    """
+    Scrape product data from the given URL using Selenium and BeautifulSoup.
 
-    product_data = {}
-    product_data['handle'] = url.split('/products/')[-1]
-    product_data['title'] = find_title(soup)
-    product_data['body'] =  find_body(soup)
-    product_data['type'] =  find_type(soup)
-    product_data['product_info'] = find_product_info(soup)
-    product_data['gender'] = find_gender(soup)
+    Args:
+        url (str): The URL of the product page.
+        driver (WebDriver): The Selenium WebDriver instance.
 
-    return product_data
+    Returns:
+        dict: A dictionary containing scraped product data.
+    """
+    try:
+        driver.get(url)
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        product_data = {
+            'url': url,
+            'handle': url.split('/products/')[-1],
+            'title': find_title(soup),
+            'body': find_body(soup),
+            'type': find_type(soup),
+            'product_info': find_product_info(soup),
+            'gender': find_gender(soup)
+        }
+
+        return product_data
+
+    except Exception as e:
+        print(f"Error scraping product data: {e}")
+        return {'url': url}
+
